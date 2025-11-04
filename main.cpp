54434f5d3d43aba3d04c170c709810e252d2c349
aea53 <aea53@cam.ac.uk>
2025-11-04 10:33:07 +0000

    Changed blink timing

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ DigitalOut myled(LED1);
 int main() {
         while(true) {
                 myled = 1; // LED is ON
-                wait(0.2); // 200 ms
+                wait(0.1); // 200 ms
                 myled = 0; // LED is OFF
-                wait(1.0); // 1 sec
+                wait(0.2)); // 1 sec
         }
 }
