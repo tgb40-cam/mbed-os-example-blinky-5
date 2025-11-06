04f9740b2c922b3ccf0ffd0e9fd78297a3c04868
tgb40-cam <tgb40@cam.ac.uk>
2025-11-06 19:28:26 +0000

    Activity 1

--- main.cpp
+++ main.cpp
@@ -5,13 +5,53 @@
 
 #include "mbed.h"
 
-DigitalOut myled(LED2);
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+DigitalOut led3(LED3);
+
+DigitalOut* leds[3] = {&led1, &led2, &led3};
+
+InterruptIn button(USER_BUTTON);
+
+Timeout button_debounce_timeout;
+float debounce_time_interval = 0.3;
+
+const int max_presses = 6;
+int press_count = 0;
+int active_led = 0;
+int led_cycle[max_presses];
+
+void onButtonStopDebouncing(void);
+
+void onButtonPress(void) {
+    if (press_count < max_presses) {
+        *leds[0] = 1; *leds[1] = 1; *leds[2] = 1;
+        led_cycle[press_count] = active_led;
+        press_count++;
+    } else {
+        *leds[0] = 0; *leds[1] = 0; *leds[2] = 0;
+    }
+    button.rise(NULL);
+    button_debounce_timeout.attach(onButtonStopDebouncing, debounce_time_interval);
+}
+
+void onButtonStopDebouncing(void) {
+    button.rise(onButtonPress);
+}
 
 int main() {
-        while(true) {
-                myled = 1; // LED is ON
-                wait(0.1); // 200 ms
-                myled = 0; // LED is OFF
-                wait(0.2); // 1 sec
-        }
+    button.rise(onButtonPress);
+    
+    while (press_count < max_presses) {
+        *leds[active_led] = 1;
+        wait(1.0);
+        active_led = (active_led + 1) % 3;
+        *leds[0] = 0; *leds[1] = 0; *leds[2] = 0;
+    }
+
+    for (int j = 0; j < max_presses; j++) {
+        *leds[led_cycle[j]] = 1;
+        wait(1.0);
+        *leds[0] = 0; *leds[1] = 0; *leds[2] = 0;
+    }
 }
